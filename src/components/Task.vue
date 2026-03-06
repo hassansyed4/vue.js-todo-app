62d90176f8f45bd3466782e039a142bf2d5f2a97
SYED HASSAN AHMED <hassan.tonk@gmail.com>
2026-03-05 23:21:47 -0600

    Initial Commit

---
+++ src/components/Task.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="task">
+      <!-- title -->
+      <div class="title">
+        <h1>To Do List</h1>
+      </div>
+      <!-- form -->
+      <div class="form">
+        <input type="text" placeholder="New Task" />
+        <button><i class="fas fa-plus"></i></button>
+      </div>
+      <!-- task lists -->
+      <div class="taskItems">
+        <ul>
+          <li v-for="task in tasks" :key="task.id">
+            <button><i class="far fa-circle"></i> {{ task.title }} </button>
+            <button><i class="far fa-trash-alt"></i></button>
+          </li>
+          
+        </ul>
+      </div>
+      <!-- buttons -->
+      <div class="clearBtns">
+        <button>Clear completed</button>
+        <button @click="clearAll">Clear all</button>
+      </div>
+      <!-- pending task -->
+      <div class="pendingTasks">
+        <span>Pending Tasks: </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: ["tasks"],
+  methods: {
+    clearAll(){
+      this.tasks = [];
+    }
+  }
+};
+</script>
